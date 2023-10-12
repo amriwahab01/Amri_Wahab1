c08d6a5892546d994f856f81c7efbb7f7743dd8d
amriwahab01 <101490168+amriwahab01@users.noreply.github.com>
2023-10-12 08:59:00 +0800

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,23 @@
+
+package latihan;
+class maha {
+        String nama;
+        int umur;
+        String alamat;
+    }
+
+public class Latihan {
+    
+
+    public static void main(String[] args) {
+        maha n = new maha();
+        n.nama ="amri";
+        n.umur = 21;
+        n.alamat = "pambusuang";
+        
+        System.out.println("nama saya" +n.nama);
+        System.out.println("umur saya" +n.umur);
+        System.out.println("alamat saya" +n.alamat);
+    }
+    
+}
