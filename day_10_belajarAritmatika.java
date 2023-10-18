51c831c14ee6b4f358166fdd8adef8ea1f0aa854
amriwahab01 <101490168+amriwahab01@users.noreply.github.com>
2023-10-19 00:56:00 +0800

    Add files via upload

---
+++ day_10_belajarAritmatika.java
@@ -0,0 +1,27 @@
+package latihan;
+
+public class day_10_belajarAritmatika {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 5;
+
+        // Penjumlahan
+        int hasilPenjumlahan = angka1 + angka2;
+        System.out.println("Penjumlahan: " + hasilPenjumlahan);
+
+        // Pengurangan
+        int hasilPengurangan = angka1 - angka2;
+        System.out.println("Pengurangan: " + hasilPengurangan);
+
+        // Perkalian
+        int hasilPerkalian = angka1 * angka2;
+        System.out.println("Perkalian: " + hasilPerkalian);
+
+        // Pembagian
+        double hasilPembagian = (double) angka1 / angka2; // Menggunakan double untuk hasil yang akurat
+        System.out.println("Pembagian: " + hasilPembagian);
+    }
+}
+
+    
+
