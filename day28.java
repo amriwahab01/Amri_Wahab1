4f9b5748d314469c8169b2b967f584360a5317bf
amriwahab01 <101490168+amriwahab01@users.noreply.github.com>
2023-11-05 22:08:22 +0800

    Add files via upload

---
+++ day28.java
@@ -0,0 +1,21 @@
+// anda sedang berbelanja dan membeli tiga barang
+//dengan harga masing-masing 
+//Rp 50.000, Rp 75.000, dan Rp 120.000.
+//Berapa total harga belanjaan Anda?
+
+package latihan;
+
+public class day28 {
+    public static void main(String[] args) {
+        //Berapa total harga belanjaan Anda?
+        int barang1 = 50000; // harga barang pertama
+        int barang2 = 75000; // harga barang kedua
+        int barang3 = 12000; // harga barang ketiga
+        
+        // lalu kita hitung barangnya
+        int barang = barang1 + barang2 + barang3;
+        
+        System.out.println("total harga belanja : " + barang);
+    }
+    
+}
