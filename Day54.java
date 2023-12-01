09e4fdc83bcc3946c43cb1d3aff4b2b36931e744
amriwahab01 <101490168+amriwahab01@users.noreply.github.com>
2023-12-01 21:35:53 +0800

    Add files via upload

---
+++ Day54.java
@@ -0,0 +1,19 @@
+package latihan;
+
+import java.util.Scanner;
+
+public class Day54 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("masukkan nilai : ");
+            int nilai = scanner.nextInt();
+            if (40 <= nilai && nilai <= 99) {
+                System.out.println("Inputan Anda: " + nilai);
+                break;
+            } else {
+                
+            }
+        }
+    }
+}
