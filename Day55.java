6cd6beacecaab09c9df932a6f38f1dae7058618d
amriwahab01 <101490168+amriwahab01@users.noreply.github.com>
2023-12-02 22:40:06 +0800

    Add files via upload

---
+++ Day55.java
@@ -0,0 +1,16 @@
+package latihan;
+
+public class Day55 {
+    public static void main(String[] args) {
+        System.out.println("Menggunakan increment dalam perulangan for:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Nilai i: " + i);
+        }
+        System.out.println("\nMenggunakan increment yang berbeda dalam perulangan for:");
+        for (int x = 0; x <= 10; x += 2) {
+            System.out.println("Nilai x: " + x);
+        
+    }
+    
+        }
+}
