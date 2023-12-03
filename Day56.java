6a1cbd67ede6c6a07fd8826d63e77a7a36f4994e
amriwahab01 <101490168+amriwahab01@users.noreply.github.com>
2023-12-03 19:43:33 +0800

    Add files via upload

---
+++ Day56.java
@@ -0,0 +1,12 @@
+package latihan;
+
+public class Day56 {
+    public static void main(String[] args) {
+        String[] nama = {"ceppi","wahida","conti","agus"};
+        System.out.println("namaku :" +nama[0]);
+        System.out.println("namaku :" +nama[1]);
+        System.out.println("namaku :" +nama[2]);
+        System.out.println("namaku :" +nama[3]);
+    }
+    
+}
