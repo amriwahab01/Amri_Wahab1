bc25ecd385cbd80818c9a77929a62a90f9cc7116
amriwahab01 <101490168+amriwahab01@users.noreply.github.com>
2023-12-07 22:56:55 +0800

    Add files via upload

---
+++ Day60.java
@@ -0,0 +1,20 @@
+package latihan;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Day60 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        ArrayList x = new ArrayList();
+        
+        x.add("amri");
+        x.add(20);
+        x.add("laki-laki");
+        x.add(2002);
+        x.add(true);
+        
+        System.out.print(x);
+        
+    }
+    
+}
