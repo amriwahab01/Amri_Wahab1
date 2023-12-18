e8340147b2b69dbe7574b9e9b7fd8550db9b1529
amriwahab01 <101490168+amriwahab01@users.noreply.github.com>
2023-12-19 00:32:34 +0800

    Add files via upload

---
+++ Day71.java
@@ -0,0 +1,15 @@
+package latihan;
+import java.util.Scanner;
+public class Day71 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.println("masukkan nilai atas :");
+        int nilai = input.nextInt();
+        
+        for (int i = 0;i < nilai;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
